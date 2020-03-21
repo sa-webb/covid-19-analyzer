544c1581f706e1907e36b95209966ae22b7feea8
sa-webb <sawebb30@gmail.com>
2020-03-21 14:40:38 -0500

    main script

--- server/app.py
+++ server/app.py
@@ -4,9 +4,6 @@ from pymongo import MongoClient
 df = pd.read_csv(
     './data/COVID-19/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv')
 
-# print(df.shape)
-# (123, 52)
-
 # Feature slice
 march_data = df[['Province/States', 'Country/Region', '3/1/2020', '3/2/2020',
                  '3/3/2020', '3/4/2020', '3/5/2020']]
@@ -17,4 +14,4 @@ col = db["march"]
 
 march_data.reset_index(inplace=True)  # Reset Index
 data_dict = march_data.to_dict("records")  # Convert to dictionary
-col.insert_one({"index": "WHO", "data": data_dict})  # insert into DB
+col.insert_one({"index": "WHO-March", "data": data_dict})  # insert into DB
