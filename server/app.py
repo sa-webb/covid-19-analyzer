89075803d16421c4bacbc6c2af4e2dd7bb09e42c
sa-webb <sawebb30@gmail.com>
2020-03-21 17:02:20 -0500

    reformat columns for web

--- server/app.py
+++ server/app.py
@@ -4,9 +4,15 @@ from pymongo import MongoClient
 df = pd.read_csv(
     './data/COVID-19/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv')
 
+df_new = df.rename(columns={'Province/States': 'province_states', 'Country/Region': 'country_region'})
+#print(df_new)
+
+
 # Feature slice
-march_data = df[['Province/States', 'Country/Region', '3/1/2020', '3/2/2020',
+march_data = df_new[['province_states', 'country_region', '3/1/2020', '3/2/2020',
                  '3/3/2020', '3/4/2020', '3/5/2020']]
+
+# print(march_data)
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["covid-19"]
