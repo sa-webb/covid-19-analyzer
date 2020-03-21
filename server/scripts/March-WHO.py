737698b05bd13611416b878f54da31d168666ba9
sa-webb <sawebb30@gmail.com>
2020-03-21 17:14:42 -0500

    refactored

--- server/scripts/March-WHO.py
+++ server/scripts/March-WHO.py
@@ -4,11 +4,12 @@ from pymongo import MongoClient
 df = pd.read_csv(
     './data/COVID-19/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv')
 
-# print(df.shape)
-# (123, 52)
+df_new = df.rename(columns={'Province/States': 'province_states', 'Country/Region': 'country_region'})
+#print(df_new)
+
 
 # Feature slice
-march_data = df[['Province/States', 'Country/Region', '3/1/2020', '3/2/2020',
+march_data = df_new[['province_states', 'country_region', '3/1/2020', '3/2/2020',
                  '3/3/2020', '3/4/2020', '3/5/2020']]
 
 client = MongoClient("mongodb://localhost:27017/")
@@ -17,4 +18,4 @@ col = db["march"]
 
 march_data.reset_index(inplace=True)  # Reset Index
 data_dict = march_data.to_dict("records")  # Convert to dictionary
-col.insert_one({"index": "WHO", "data": data_dict})  # inesrt into DB
+col.insert_one({"index": "WHO-March", "data": data_dict})  # insert into DB
