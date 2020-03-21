2aca8b7d461f09ecbbbe2a27b0da66152ad9bc65
sa-webb <sawebb30@gmail.com>
2020-03-21 17:53:24 -0500

    staging

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SingleApiHook from './components/SingleApiHook';
 import ReuseableHook from './components/ReuseableHook';
 import Page404 from './pages/Page404';
+import Dashboard from './temp/example/Dashboard';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={SingleApiHook} />
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/single" component={SingleApiHook} />
         <Route exact path="/reuse" component={ReuseableHook} />
         <Route component={Page404} />
       </Switch>
