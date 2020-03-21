e1cdf25360f39f23be90382dcc79c8a8c63446fd
sa-webb <sawebb30@gmail.com>
2020-03-21 17:00:52 -0500

    removed

--- server/src/export/api/index.js
+++
@@ -1,8 +0,0 @@
-// get_all = async (req, res) => {
-//   const data = await collection(coll).find({});
-//   await res.json(data);
-// };
-
-// module.exports = {
-//   get_all
-// }
--- server/src/export/routes/data.routes.js
+++
@@ -1,9 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const data_controller = require("../api/index")
-
-// router.get('/', data_controller.get_all);
-
-// module.exports = router;
-
