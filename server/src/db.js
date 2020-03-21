d94f1fdad691386b9e68de620088ce2a638dc4d9
sa-webb <sawebb30@gmail.com>
2020-03-21 17:32:11 -0500

    cleaned

--- server/src/db.js
+++ server/src/db.js
@@ -1,23 +1,23 @@
-const { MongoClient, ObjectId } = require('mongodb')
-const options = require('../config/index.js')
-const connectionUrl = 'mongodb://localhost:27017'
-const dbName = 'covid-19'
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
+const options = require('../config/index.js');
 
-let db
+let db;
 
 const init = () =>
-  MongoClient.connect(connectionUrl, options).then((client) => {
-    db = client.db(dbName)
-  })
+  MongoClient.connect(process.env.MONGO, options).then(client => {
+    db = client.db(process.env.DB);
+  });
 
 const getAllArray = () => {
-  const collection = db.collection('march')
-  return collection.find({}).toArray()
-}
+  const collection = db.collection('march');
+  return collection.find({}).toArray();
+};
 
 const getAll = () => {
-  const collection = db.collection('march')
-  return collection.findOne({})
-}
+  const collection = db.collection('march');
+  return collection.findOne({});
+};
 
-module.exports = { init, getAll, getAllArray }
+module.exports = { init, getAll, getAllArray };
