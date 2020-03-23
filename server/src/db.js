c8898e0c16f8ca10eddb12c1859d2e439accc47c
sa-webb <sawebb30@gmail.com>
2020-03-22 23:39:39 -0500

    temp

--- server/src/db.js
+++ server/src/db.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const options = require('../config/index.js');
 
+const { us_total_confirmed } = require('./aggregations')
+
 let db;
 
 const init = () =>
@@ -11,17 +13,77 @@ const init = () =>
   });
 
 const getAllArray = () => {
-  const collection = db.collection('march');
-  return collection.find({}).toArray();
-};
-
-const getMarch = () => {
-  const collection = db.collection('march');
-  return collection.findOne({});
-};
-const getAll = () => {
   const collection = db.collection('WHO');
-  return collection.findOne({});
+  return collection.find({ index: 'WHO-All' }).toArray();
 };
 
-module.exports = { init, getAll, getAllArray, getMarch };
+const getMarchWHO = () => {
+  const collection = db.collection('march');
+  return collection.findOne({ index: 'WHO-March' });
+};
+
+const getAllWHO = () => {
+  const collection = db.collection('WHO');
+  return collection.findOne({ index: 'WHO-All' });
+};
+
+const getAllConfirmedCSSE = () => {
+  const collection = db.collection('csse');
+  return collection.findOne({ index: 'csse-all-confirmed' });
+};
+
+const usConfirmedTotal = async (req, res) => {
+  const collection = db.collection('csse');
+  return collection
+    .aggregate(us_total_confirmed)
+    .toArray();
+};
+
+const getUSConfirmedCSSE = async (req, res) => {
+  const collection = db.collection('csse');
+  const results = collection.aggregate(
+    [
+      {
+        $match: {
+          index: 'csse-all-confirmed'
+        }
+      },
+      {
+        $unwind: {
+          path: '$data'
+        }
+      },
+      {
+        $match: {
+          'data.country_region': 'US'
+        }
+      },
+      {
+        $group: {
+          _id: 1,
+          total: {
+            $sum: '$data.3/21/20'
+          }
+        }
+      }
+    ],
+    function(err, results) {
+      if (err) {
+        res.json(500, err);
+      } else {
+        res.json(results);
+      }
+    }
+  );
+  console.log(results);
+};
+
+module.exports = {
+  init,
+  getAllWHO,
+  getAllArray,
+  getMarchWHO,
+  getAllConfirmedCSSE,
+  getUSConfirmedCSSE,
+  usConfirmedTotal
+};
