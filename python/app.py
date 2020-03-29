4f6337ab546512e1eae2e1ce7db337055ed76692
sa-webb <sawebb30@gmail.com>
2020-03-29 17:59:21 -0500

    added dotenv

--- python/app.py
+++ python/app.py
@@ -1,25 +1,26 @@
+import os
 import pandas as pd
+from dotenv import load_dotenv
 from pymongo import MongoClient
+
+load_dotenv('.env')
+print(os.getenv('MONGO_URI'))  # Bar
 
 df = pd.read_csv(
     './data/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 
 # print(df)
 
-df_cleansed = df.rename(columns={'Province/States': 'province_states', 'Country/Region': 'country_region'})
+df_cleansed = df.rename(
+    columns={'Province/States': 'province_states', 'Country/Region': 'country_region'})
 print(df_cleansed)
 
-# Feature slice
-march_data = df_cleansed[['province_states', 'country_region', '3/1/2020', '3/2/2020','3/3/2020', '3/4/2020', '3/5/2020']]
-
-# print(march_data)
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["covid-19"]
+client = MongoClient(os.getenv('MONGO_URI'))
+db = client[os.getenv('DB')]
 col = db["csse"]
 
 df_cleansed.reset_index(inplace=True)  # Reset Index
 data_dict = df_cleansed.to_dict("records")  # Convert to dictionary
-col.insert_one({"index": "csse-all-recovered", "data": data_dict})  # insert into DB
+col.insert_one({"index": "csse-all", "data": data_dict})  # insert into DB
 
 print('inserted data')
