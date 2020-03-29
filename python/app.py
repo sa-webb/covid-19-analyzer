a728e9bcbce83e26c2bfe3b3df50ba1372b7aeb6
sa-webb <sawebb30@gmail.com>
2020-03-29 17:39:24 -0500

    temp

--- python/app.py
+++ python/app.py
@@ -2,23 +2,24 @@ import pandas as pd
 from pymongo import MongoClient
 
 df = pd.read_csv(
-    './data/COVID-19/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv')
+    './data/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 
-df_new = df.rename(columns={'Province/States': 'province_states', 'Country/Region': 'country_region'})
-print(df_new)
+# print(df)
 
+df_cleansed = df.rename(columns={'Province/States': 'province_states', 'Country/Region': 'country_region'})
+print(df_cleansed)
 
 # Feature slice
-# march_data = df_new[['province_states', 'country_region', '3/1/2020', '3/2/2020','3/3/2020', '3/4/2020', '3/5/2020']]
+march_data = df_cleansed[['province_states', 'country_region', '3/1/2020', '3/2/2020','3/3/2020', '3/4/2020', '3/5/2020']]
 
 # print(march_data)
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["covid-19"]
-col = db["WHO"]
+col = db["csse"]
 
-df_new.reset_index(inplace=True)  # Reset Index
-data_dict = df_new.to_dict("records")  # Convert to dictionary
-col.insert_one({"index": "WHO-All", "data": data_dict})  # insert into DB
+df_cleansed.reset_index(inplace=True)  # Reset Index
+data_dict = df_cleansed.to_dict("records")  # Convert to dictionary
+col.insert_one({"index": "csse-all-recovered", "data": data_dict})  # insert into DB
 
 print('inserted data')
