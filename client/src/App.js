85dd1f7fa527474c0c876d2399df9caf5cb5c38a
sa-webb <sawebb30@gmail.com>
2020-03-30 12:13:57 -0500

    temp

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SingleApiHook from './components/SingleApiHook';
 import ReuseableHook from './components/ReuseableHook';
 import Page404 from './pages/Page404';
-import Dashboard from './temp/example/Dashboard';
+import MockDashboard from './temp/example/Dashboard';
+import Main from './temp/devoid/Main';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={Main} />
+        <Route exact path="/mock" component={MockDashboard} />
         <Route exact path="/single" component={SingleApiHook} />
         <Route exact path="/reuse" component={ReuseableHook} />
         <Route component={Page404} />
