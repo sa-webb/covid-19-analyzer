9fc62c29f198f75b16dc5c9adca55e64fba28fc1
sa-webb <sawebb30@gmail.com>
2020-03-30 19:42:48 -0500

    removed mock

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SingleApiHook from './components/SingleApiHook';
 import ReuseableHook from './components/ReuseableHook';
 import Page404 from './pages/Page404';
-import MockDashboard from './temp/example/Dashboard';
 import Main from './temp/devoid/Main';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Main} />
-        <Route exact path="/mock" component={MockDashboard} />
         <Route exact path="/single" component={SingleApiHook} />
         <Route exact path="/reuse" component={ReuseableHook} />
         <Route component={Page404} />
