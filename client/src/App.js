2743300881b36f3d3f86221d259601c655b8da01
sa-webb <sawebb30@gmail.com>
2020-03-30 21:33:18 -0500

    dynamic chart

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import SingleApiHook from './components/SingleApiHook';
 import ReuseableHook from './components/ReuseableHook';
 import Page404 from './pages/Page404';
 import Main from './temp/devoid/Main';
+import DynamicChart from './temp/figura/Chart.jsx';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Main} />
+        <Route exact path="/chart" component={DynamicChart} />
         <Route exact path="/single" component={SingleApiHook} />
         <Route exact path="/reuse" component={ReuseableHook} />
         <Route component={Page404} />
