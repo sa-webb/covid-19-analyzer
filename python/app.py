0d8c5d71364654a121c1463e4367437ea7879e6e
sa-webb <sawebb30@gmail.com>
2020-03-30 20:39:48 -0500

    temp

--- python/app.py
+++ python/app.py
@@ -7,12 +7,12 @@ load_dotenv('.env')
 print(os.getenv('MONGO_URI'))
 
 df = pd.read_csv(
-    './data/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
+    './data/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 
 print(df)
 
 df_cleansed = df.rename(
-    columns={'Province/States': 'province_states', 'Country/Region': 'country_region'})
+    columns={'Province/State': 'province_state', 'Country/Region': 'country_region'})
 print(df_cleansed)
 
 client = MongoClient(os.getenv('MONGO_URI'))
@@ -21,6 +21,6 @@ col = db["csse"]
 
 df_cleansed.reset_index(inplace=True)  # Reset Index
 data_dict = df_cleansed.to_dict("records")  # Convert to dictionary
-col.insert_one({"index": "csse-all-recovered", "current_date": "3/30/20", "data": data_dict})  # insert into DB
+col.insert_one({"index": "csse-all-confirmed", "current_date": "3/30/20", "data": data_dict})  # insert into DB
 
 print('inserted data')
