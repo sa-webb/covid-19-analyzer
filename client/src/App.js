f3a6ba6637e23d5c82c9c832d4b9b8a78433e2d2
sa-webb <sawebb30@gmail.com>
2020-03-31 22:36:16 -0500

    cleaned

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import Main from './components/Main';
 import Page404 from './pages/Page404';
-import DynamicChart from './temp/figura/Chart.jsx';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Main} />
-        <Route exact path="/chart" component={DynamicChart} />
         <Route component={Page404} />
       </Switch>
     </Router>
