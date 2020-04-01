a94ace4ad3cc267136bf733e319ad36bc5a22c31
sa-webb <sawebb30@gmail.com>
2020-03-31 21:46:20 -0500

    v1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import SingleApiHook from './components/SingleApiHook';
-import ReuseableHook from './components/ReuseableHook';
-import AllPromise from './components/Main';
+
+import Main from './components/Main';
 import Page404 from './pages/Page404';
-import Main from './temp/devoid/Main';
 import DynamicChart from './temp/figura/Chart.jsx';
 
 function App() {
@@ -12,10 +10,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Main} />
-        <Route exact path="/promise" component={AllPromise} />
         <Route exact path="/chart" component={DynamicChart} />
-        <Route exact path="/single" component={SingleApiHook} />
-        <Route exact path="/reuse" component={ReuseableHook} />
         <Route component={Page404} />
       </Switch>
     </Router>
