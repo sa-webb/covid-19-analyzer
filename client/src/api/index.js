a78eedd91202cd36ae47773eacb2ae9c35098f25
sa-webb <sawebb30@gmail.com>
2020-03-31 21:46:27 -0500

    temp

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,9 +7,37 @@ const api = axios.create({
 export const getAllData = payload => api.get(`/data`);
 export const getUsGrowthCurve = payload => api.get(`/us-growth-curve`);
 
+export const getCSSE = () => {
+  const promise1 = payload => api.get(`/data`)
+  const promise2 = payload => api.get(`/us-growth-rate`)
+  try {
+    Promise.all([promise1, promise2]).then(function(values) {
+      console.log(values[0].data[0].values);
+      console.log(values[1].data.data);
+    });
+  } catch (error) {
+    alert(error)
+  }
+}
+
+async function AllPromise() {
+  try {
+    const response = await Promise.all([
+      axios.get('http://localhost:5000/us-growth-curve'),
+      axios.get('http://localhost:5000/data')
+    ]);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    alert(error); // catches both errors
+  }
+}
+
 const apis = {
   getAllData,
-  getUsGrowthCurve
+  getUsGrowthCurve,
+  AllPromise
 };
 
 export default apis;
