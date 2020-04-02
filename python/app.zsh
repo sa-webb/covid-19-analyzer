cb9f273dd936c9f4861a4b3a12cffda57aad46c7
sa-webb <sawebb30@gmail.com>
2020-04-02 07:47:21 -0500

    initial

---
+++ python/app.zsh
@@ -0,0 +1,5 @@
+python ./scripts/global-confirmed-csse.py
+python ./scripts/global-recovered-csse.py
+python ./scripts/global-deaths-csse.py
+python ./scripts/us-confirmed-csse.py
+python ./scripts/us-deaths-csse.py
