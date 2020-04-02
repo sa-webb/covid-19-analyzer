7213f399a015da303c4adf765039655ada7906cc
sa-webb <sawebb30@gmail.com>
2020-04-01 23:01:49 -0500

    cleaned

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,3 +1,3 @@
-export const US_CONFIRMED = '/us-confirmed-growth-curve/'
-export const US_RECOVERED = '/us-recovered-growth-curve/'
-export const US_DEATHS = '/us-deaths-growth-curve/'
+export const US_CONFIRMED = 'us-confirmed-growth-curve/'
+export const US_RECOVERED = 'us-recovered-growth-curve/'
+export const US_DEATHS = 'us-deaths-growth-curve/'
