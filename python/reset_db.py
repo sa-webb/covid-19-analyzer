601adb224f94313ef120603b752da9361fd324d2
sa-webb <sawebb30@gmail.com>
2020-04-04 22:10:19 -0500

    migrated

--- python/reset_db.py
+++ python/reset_db.py
@@ -3,9 +3,8 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 
 load_dotenv('.env')
-
 client = MongoClient(os.getenv('MONGO_URI'))
 db = client[os.getenv('DB')]
 col = db["csse"]
 
-db.col.deleteMany({})
+col.delete_many({})
