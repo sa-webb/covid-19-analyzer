c6cffc5f8bb3083e7ef73749ffc9927f07da4185
sa-webb <sawebb30@gmail.com>
2020-04-04 22:13:54 -0500

    final

--- python/scripts/csse/global-deaths-csse.py
+++ python/scripts/csse/global-deaths-csse.py
@@ -1,25 +1,27 @@
 import os
+import datetime
 import pandas as pd
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-load_dotenv('.env')
-
 df = pd.read_csv(
     './data/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
 
-print(df)
+df_cleansed = df.rename(
+    columns={'Province/State': 'province_state', 'Country/Region': 'country_region'})
 
-# df_cleansed = df.rename(columns={'Province/States': 'province_states', 'Country/Region': 'country_region'})
+load_dotenv('.env')
+client = MongoClient(os.getenv('MONGO_URI'))
+db = client[os.getenv('DB')]
+col = db["csse"]
 
-# # print(df_cleansed)
+df_cleansed.reset_index(inplace=True)  # Reset Index
+data_dict = df_cleansed.to_dict("records")  # Convert to dictionary
 
-# client = MongoClient(os.getenv('MONGO_URI'))
-# db = client[os.getenv('DB')]
-# col = db["csse"]
 
-# df_cleansed.reset_index(inplace=True)  # Reset Index
-# data_dict = df_cleansed.to_dict("records")  # Convert to dictionary
-# col.insert_one({"index": "csse-all", "data": data_dict})  # insert into DB
+x = datetime.datetime.now()
+current_date = x.strftime("%D")
 
-# print('success')
+col.insert_one({"index": "csse-all-deaths", "current_date": current_date, "data": data_dict})  # insert into DB
+
+print('inserted data')
--- python/scripts/csse/us-deaths-csse.py
+++ python/scripts/csse/us-deaths-csse.py
@@ -1,12 +1,27 @@
 import os
+import datetime
 import pandas as pd
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-load_dotenv('.env')
-print(os.getenv('MONGO_URI'))
-
 df = pd.read_csv(
     './data/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv')
 
-print(df)
+df_cleansed = df.rename(
+    columns={'Province/State': 'province_state', 'Country/Region': 'country_region'})
+
+load_dotenv('.env')
+client = MongoClient(os.getenv('MONGO_URI'))
+db = client[os.getenv('DB')]
+col = db["csse"]
+
+df_cleansed.reset_index(inplace=True)  # Reset Index
+data_dict = df_cleansed.to_dict("records")  # Convert to dictionary
+
+
+x = datetime.datetime.now()
+current_date = x.strftime("%D")
+
+col.insert_one({"index": "csse-all-us-deaths", "current_date": current_date, "data": data_dict})  # insert into DB
+
+print('inserted data')
