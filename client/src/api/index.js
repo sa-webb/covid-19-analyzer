fbdcc54f24ac8845eef4a3f6febbef0760bdef5a
sa-webb <sawebb30@gmail.com>
2020-04-21 21:40:54 -0500

    cleaned

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -15,8 +15,8 @@ export const getAllUs = () => {
   try {
     Promise.all([usConfirmed, usRecovered, usDeaths]).then(function(values) {
       console.log(values[0].data[0].values);
-      console.log(values[1].data.data);
-      console.log(values[2].data.data);
+      console.log(values[1].data[0].data);
+      console.log(values[2].data[0].data);
     });
   } catch (error) {
     alert(error);
